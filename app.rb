9a34ccdefa6ef6272a3d2e13d57a04ef8a8ce7dd
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-12 03:52:53 +0000

    Finished dice roll page

--- app.rb
+++ app.rb
@@ -10,5 +10,13 @@ get("/") do
 end
 
 get("/process_roll") do
-  
+  @dice = params.fetch("dice").to_i
+  @sides = params.fetch("sides").to_i
+  @rolls = []
+  @dice.times do
+    die = rand(1..@sides)
+    @rolls.push(die)
+  end
+
+  erb(:process_roll)
 end
